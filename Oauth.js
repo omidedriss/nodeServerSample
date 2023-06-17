a7198e26aa65dfccb4b8f5c32428866c2580709e
omidedriss <omidedriss@DESKTOP-7OUEIQN>
2023-06-17 15:06:14 +0330

    add

--- Oauth.js
+++ Oauth.js
@@ -1,2 +1,3 @@
 //https://quera.org/blog/an-introdution-to-oauth2/
 //npm i axios-oauth-client
+//https://virgool.io/@farzaneh.parvar/%D8%A8%D8%A7-oauth2-%D8%A2%D8%B4%D9%86%D8%A7-%D8%B4%D9%88%DB%8C%D8%AF-p4k9fjaljzva
